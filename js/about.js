7d5aeca26d418e19995a77840bf63f9bff3c1505
Nikitha C <nikithac@Nikithas-MacBook-Pro.local>
2026-05-27 12:44:54 +0530

    Reframe Kathak photo: reduce zoom to 1.2 and shift object-position to 58% to show dancer's full body including legs

--- js/about.js
+++ js/about.js
@@ -83,7 +83,7 @@
     if (polaroidFrame && polaroidImg && polaroidCaption) {
         const hoverImages = [
             { src: 'assets/images/nikitha-polaroid.jpg?v=3', caption: '#goldenhour<br>#office vibes' },
-            { src: 'assets/images/nikitha-polaroid-2.jpg', caption: '#kathak<br>#dancelife', zoom: 1.35, objectPosition: 'center 40%' },
+            { src: 'assets/images/nikitha-polaroid-2.jpg', caption: '#kathak<br>#dancelife', zoom: 1.2, objectPosition: 'center 58%' },
             { src: 'assets/images/nikitha-polaroid-3.png', caption: '#nature walk<br>#weekend vibes' },
             { src: 'assets/images/nikitha-polaroid-4.png', caption: '#cozy coffee<br>#morning fuel' }
         ];
